bd5e4c0374a40e4c06f17b56345785104a80ed2c
victorbischoff <morkebergv@gmail.com>
2023-02-19 19:20:45 +0100

    added leader w to save

--- lua/victor/remap.lua
+++ lua/victor/remap.lua
@@ -6,5 +6,5 @@ vim.keymap.set('', '<down>', '<nop>')
 vim.keymap.set('', '<left>', '<nop>')
 vim.keymap.set('', '<right>', '<nop>')
 --Vim remaps
-vim.keymap.set('n', '<C-s>', ':update<cr>')
+vim.keymap.set('n', '<leader>w', '<CMD>update<CR>')
 
